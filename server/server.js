0fd4e8d21599bbf340f70accacbaf7b72128c322
Devashish Shrestha <devashreeganesha108@gmail.com>
2018-05-09 01:13:20 -0700

    CORS

--- server/server.js
+++ server/server.js
@@ -51,8 +51,13 @@ storage.getBuckets()
             console.error('ERROR:', err);
        });
 
-
 //app.use(cors())
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, '../client/build')));
